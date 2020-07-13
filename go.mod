4d2b2cea6341170867193ea15ce262e939115c4b
tchughesiv <tchughesiv@gmail.com>
2020-07-13 14:32:29 -0500

    init
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- go.mod
+++ go.mod
@@ -5,5 +5,10 @@ go 1.13
 require (
 	github.com/containers/image v3.0.2+incompatible
 	github.com/containers/image/v5 v5.5.1
+	github.com/containers/storage v1.20.2
+	github.com/opencontainers/go-digest v1.0.0
+	github.com/opencontainers/image-spec v1.0.2-0.20190823105129-775207bd45b6
 	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.6.0
+	k8s.io/client-go v11.0.0+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -3,25 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/containers/image/v5/image"
-	"github.com/containers/image/v5/transports/alltransports"
-	imagetypes "github.com/containers/image/v5/types"
+	"github.com/containers/image/image"
+	is "github.com/containers/image/storage"
+	"github.com/containers/image/transports/alltransports"
+	"github.com/containers/image/types"
+	"github.com/containers/storage"
 	pkgerrors "github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	var debug bool
 	println("test")
+	logrus.SetLevel(logrus.ErrorLevel)
+	if !debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
 	if err := imageLookup(); err != nil {
-		println(err)
+		logrus.Error(err)
 	}
 }
 
 // test product type/version aggregation from pod image lookup
 func imageLookup() (retErr error) {
 	images := []string{"quay.io/crio/redis:alpine", "quay.io/crio/redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55"}
-	imgCtx := &imagetypes.SystemContext{
+	imgCtx := &types.SystemContext{
 		OSChoice: "linux",
 	}
 	ctx := context.Background()
@@ -29,6 +38,34 @@ func imageLookup() (retErr error) {
 	ctx, cancel = context.WithTimeout(ctx, time.Duration(10)*time.Second)
 	defer cancel()
 
+	storeOptions, err := storage.DefaultStoreOptionsAutoDetectUID()
+	if err != nil {
+		return err
+	}
+	store, err := storage.GetStore(storeOptions)
+	if err != nil {
+		return err
+	}
+	errors := false
+	defer func() {
+		store.Shutdown(false)
+		if errors {
+			os.Exit(1)
+		}
+	}()
+
+	ref, err := is.Transport.ParseStoreReference(store, "test")
+	if err != nil {
+		return err
+	}
+	strRef := ref.StringWithinTransport()
+	imgRef, err := is.Transport.ParseReference(strRef)
+	if err != nil {
+		return err
+	}
+	if imgRef == nil {
+		return err
+	}
 	imagesCount := uniqueCount(images)
 	for img, num := range imagesCount {
 		fmt.Println(img)
@@ -38,7 +75,7 @@ func imageLookup() (retErr error) {
 			return err
 		}
 		defer func() {
-			if err := imgSrc.Close(); err != nil {
+			if err = imgSrc.Close(); err != nil {
 				retErr = pkgerrors.Wrapf(retErr, fmt.Sprintf("(could not close image: %v) ", err))
 			}
 		}()
@@ -64,7 +101,7 @@ func imageLookup() (retErr error) {
 	return retErr
 }
 
-func parseImageSource(ctx context.Context, imgCtx *imagetypes.SystemContext, name string) (imagetypes.ImageSource, error) {
+func parseImageSource(ctx context.Context, imgCtx *types.SystemContext, name string) (types.ImageSource, error) {
 	ref, err := alltransports.ParseImageName(name)
 	if err != nil {
 		return nil, err
