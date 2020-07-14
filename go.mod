fe450ff04a94cdb915fb32ace33e219ee2634fa2
Tommy Hughes IV <tchughesiv@gmail.com>
2020-07-14 15:11:11 -0500

    dev (#1)
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * fixes
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>
    
    * init
    
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/tchughesiv/image-labels-test
+
+go 1.13
+
+require (
+	github.com/containers/image/v5 v5.5.1
+	github.com/containers/storage v1.20.2
+	github.com/sirupsen/logrus v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/containers/image/v5/image"
+	is "github.com/containers/image/v5/storage"
+	"github.com/containers/image/v5/types"
+	"github.com/containers/storage"
+	"github.com/sirupsen/logrus"
+)
+
+// var images = []string{"quay.io/crio/redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55", "redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55"}
+
+func main() {
+	debug := true
+	args := os.Args[1:]
+	logrus.SetLevel(logrus.ErrorLevel)
+	if debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	if err := imageLookup(args); err != nil {
+		logrus.Error(err)
+	}
+}
+
+// test product type/version aggregation from pod image lookup
+func imageLookup(args []string) (retErr error) {
+	ctx := context.Background()
+	//var cancel context.CancelFunc = func() {}
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(10)*time.Second)
+	defer cancel()
+	storeOptions, err := storage.DefaultStoreOptionsAutoDetectUID()
+	if err != nil {
+		return err
+	}
+	store, err := storage.GetStore(storeOptions)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if _, err := store.Shutdown(false); err != nil {
+			logrus.Error(err)
+			os.Exit(1)
+		}
+	}()
+	/*
+		imgs, err := store.Images()
+		if err != nil {
+			return err
+		}
+		for _, i := range imgs {
+			println("digest = " + i.Digest.String())
+		}
+	*/
+	for _, img := range args {
+		println()
+		println(img)
+		ref, err := is.Transport.ParseStoreReference(store, img)
+		if err != nil {
+			return err
+		}
+		strRef := ref.StringWithinTransport()
+		imgRef, err := is.Transport.ParseReference(strRef)
+		if err != nil {
+			return err
+		}
+		if imgRef == nil {
+			return err
+		}
+		println(imgRef.DockerReference().Name())
+		imgCtx := &types.SystemContext{
+			OSChoice: "linux",
+		}
+		imgSrc, err := imgRef.NewImageSource(ctx, imgCtx)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err = imgSrc.Close(); err != nil {
+				retErr = err
+			}
+		}()
+		img, err := image.FromUnparsedImage(ctx, imgCtx, image.UnparsedInstance(imgSrc, nil))
+		if err != nil {
+			return fmt.Errorf("Error parsing manifest for image: %v", err)
+		}
+		config, err := img.OCIConfig(ctx)
+		if err != nil {
+			return fmt.Errorf("Error reading OCI-formatted configuration data: %v", err)
+		}
+		println(config.Config.User)
+		inspectInfo, err := img.Inspect(ctx)
+		if err != nil {
+			return err
+		}
+		println(inspectInfo.Tag)
+		println("IMAGE LABELS -")
+		for key, val := range inspectInfo.Labels {
+			if key == "org.jboss.product" {
+				println(key + "=" + val)
+			}
+		}
+	}
+	println()
+	return retErr
+}
