ef66b2f976abf3ecb6c7f4abb5565bda25b514d6
tchughesiv <tchughesiv@gmail.com>
2020-07-14 12:55:45 -0500

    init
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- main.go
+++ main.go
@@ -6,21 +6,22 @@ import (
 	"os"
 	"time"
 
-	"github.com/containers/image/image"
-	is "github.com/containers/image/storage"
-	"github.com/containers/image/transports/alltransports"
-	"github.com/containers/image/types"
+	"github.com/containers/image/v5/image"
+	is "github.com/containers/image/v5/storage"
+	"github.com/containers/image/v5/transports/alltransports"
+	"github.com/containers/image/v5/types"
 	"github.com/containers/storage"
 	pkgerrors "github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
+var images = []string{"quay.io/crio/redis:alpine", "quay.io/crio/redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55", "redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55"}
+
 func main() {
-	var debug bool
-	debug = true
+	debug := true
 	println("test")
 	logrus.SetLevel(logrus.ErrorLevel)
-	if !debug {
+	if debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
 	if err := imageLookup(); err != nil {
@@ -30,30 +31,25 @@ func main() {
 
 // test product type/version aggregation from pod image lookup
 func imageLookup() (retErr error) {
-	images := []string{"quay.io/crio/redis:alpine", "quay.io/crio/redis@sha256:1780b5a5496189974b94eb2595d86731d7a0820e4beb8ea770974298a943ed55"}
 	ctx := context.Background()
-	var cancel context.CancelFunc = func() {}
-	ctx, cancel = context.WithTimeout(ctx, time.Duration(10)*time.Second)
+	//var cancel context.CancelFunc = func() {}
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(10)*time.Second)
 	defer cancel()
-	storeOptions, err := storage.DefaultStoreOptions(false, 0)
+	storeOptions, err := storage.DefaultStoreOptionsAutoDetectUID()
 	if err != nil {
 		return err
 	}
-	storeOptions.GraphDriverName = "overlay"
 	println(storeOptions.GraphDriverName)
-
 	store, err := storage.GetStore(storeOptions)
 	if err != nil {
 		return err
 	}
-	errors := false
 	defer func() {
-		store.Shutdown(false)
-		if errors {
+		if _, err := store.Shutdown(false); err != nil {
+			logrus.Error(err)
 			os.Exit(1)
 		}
 	}()
-
 	imgs, err := store.Images()
 	if err != nil {
 		return err
