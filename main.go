123e227ae9826b7defb742018f9f44c1f99fb27b
tchughesiv <tchughesiv@gmail.com>
2020-07-16 15:16:14 -0500

    switch to buildah
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- main.go
+++ main.go
@@ -63,7 +63,6 @@ func init() {
 		logrus.Errorf(err.Error())
 		os.Exit(1)
 	}
-	// RUN sed -i -e 's|^#mount_program|mount_program|g' -e '/additionalimage.*/a "/var/lib/shared",' -e 's|^mountopt[[:space:]]*=.*$|mountopt = "nodev,fsync=0"|g' /etc/containers/storage.conf
 
 	if len(storageOptions.GraphDriverOptions) > 0 {
 		optionSlice := storageOptions.GraphDriverOptions[:]
